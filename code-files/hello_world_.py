befde5d9c768a56acf33d7db0bc6dc1b9e84f983
harsh9t <harsh9t@gmail.com>
2017-07-16 09:34:19 -0700

    hello world + variables

--- code-files/hello_world_.py
+++ code-files/hello_world_.py
@@ -1,1 +1,7 @@
 print "hello world"
+z = 56
+mystring = "hello"
+# testing code
+if mystring == "hello":
+    #print('String: %s %z' [%mystring %z])
+    print("Total score for %s is %s " % (mystring, z))

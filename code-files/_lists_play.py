17ebdee62b4237ece452d074cb8a5a90758794a9
harsh9t <harsh9t@gmail.com>
2017-07-20 17:04:08 -0700

    brushing up python lists

---
+++ code-files/_lists_play.py
@@ -0,0 +1,16 @@
+__author__ = "Harsh Thakkar"
+__license__ = "Creative Commons Attribution license (CC BY 4.0)"
+__email__ = "hthakkar@uni-bonn.de"
+__status__ = "Production"
+
+#A list with odd numbers
+mylist = []
+for i in range(0, 1000):
+    mylist.append(2*i+1)
+print mylist
+
+#exponenting in lists
+print pow(9,4)
+
+#duplicating patterns in lists
+print [3,6,9] * 2
